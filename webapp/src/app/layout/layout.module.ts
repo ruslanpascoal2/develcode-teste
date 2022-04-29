95eeffa9983d0f8b4c8d7428e16b4332e1231225
Ruslan <61955096+ruslanpascoal2@users.noreply.github.com>
2022-04-29 15:28:01 -0300

    feat: layout and shared module

---
+++ webapp/src/app/layout/layout.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    RouterModule
+  ],
+  exports: [
+    NavbarComponent
+  ],
+})
+export class LayoutModule { }
---
+++ webapp/src/app/shared/shared.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from "@angular/material/icon"
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers:[
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+  ]
+})
+export class SharedModule { }
